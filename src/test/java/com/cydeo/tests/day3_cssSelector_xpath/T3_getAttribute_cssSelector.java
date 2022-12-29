2b8deebd429cb559a6b76017c52fb38d7fd9b1e1
Mevlut Doger <mdoger@hotmail.com>
2022-12-29 02:30:31 +0000

    day3 Task getAttribute_cssSelector

--- src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T3_getAttribute_cssSelector.java
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T3_getAttribute_cssSelector.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day3_cssSelector_xpath;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class T3_getAttribute_cssSelector {
     public static void main(String[] args) {
@@ -11,6 +13,14 @@ public class T3_getAttribute_cssSelector {
         //2- Go to: https://login1.nextbasecrm.com/
         driver.get("https://login1.nextbasecrm.com");
         //3- Verify “Log in” button text is as expected:
+        //WebElement signInButton=driver.findElement(By.className("login-btn"));
+        WebElement signInButton=driver.findElement(By.cssSelector("input[type='submit'"));
+        String expectedButtonText="Log In";
+        String actualButtonText=signInButton.getAttribute("value");
+        System.out.println(actualButtonText);
+
+
+
         //Expected: Log In
 
     }
