789cd5a9c6cc360900dc4f459ca21dc99edf80b6
Mevlut Doger <mdoger@hotmail.com>
2023-01-02 04:53:12 +0000

    day4 Task cssSelector_getText

--- src/test/java/com/cydeo/tests/day4_findElwmwnts_checkBoxes_radio/T4_findElements.java
+++ src/test/java/com/cydeo/tests/day4_findElwmwnts_checkBoxes_radio/T4_findElements.java
@@ -11,18 +11,22 @@ public class T4_findElements {
     public static void main(String[] args) {
         //TC #4: FindElements Task
         //1- Open a Chrome browser
-        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
-       //2- Go to: https://practice.cydeo.com/abtest
+        //2- Go to: https://practice.cydeo.com/abtest
         driver.get("https://practice.cydeo.com/abtest");
         //3- Locate all the links in the page.
         List<WebElement> allLinks = driver.findElements(By.tagName("a"));
         //4- Print out the number of the links on the page.
         System.out.println("allLinks.size() = " + allLinks.size());
         // 5- Print out the texts of the links.
+        //6- Print out the HREF attribute values of the links
         for (WebElement each : allLinks) {
-            System.out.println("Text of link: "+ each.getText());
+            System.out.println("Text of link: " + each.getText());
+            System.out.println("HREF Values: " + each.getAttribute("href"));
         }
         //6- Print out the HREF attribute values of the links
+
+
     }
 }
