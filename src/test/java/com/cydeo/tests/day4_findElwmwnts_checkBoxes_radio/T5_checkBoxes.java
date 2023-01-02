c2753c6fdec7f76b5be924c5ee0fe55a9c9030c4
Mevlut Doger <mdoger@hotmail.com>
2023-01-02 05:38:02 +0000

    day4 Task cssSelector_getText

--- src/test/java/com/cydeo/tests/day4_findElwmwnts_checkBoxes_radio/T5_checkBoxes.java
+++ src/test/java/com/cydeo/tests/day4_findElwmwnts_checkBoxes_radio/T5_checkBoxes.java
@@ -18,9 +18,9 @@ public class T5_checkBoxes {
         WebElement checkbox1 = driver.findElement(By.xpath("//input[@name='checkbox1']"));
         WebElement checkbox2 = driver.findElement(By.xpath("//input[@name='checkbox2']"));
         //2. Confirm checkbox #1 is NOT selected by default
-        System.out.println("checkbox1.isSelected() = " + checkbox1.isSelected());
+        System.out.println("checkbox1.isSelected(), expected false = " + checkbox1.isSelected());
         // 3. Confirm checkbox #2 is SELECTED by default.
-        System.out.println("checkbox2.isSelected() = " + checkbox2.isSelected());
+        System.out.println("checkbox2.isSelected(), expected true = " + checkbox2.isSelected());
 
 
         //4. Click checkbox #1 to select it.
