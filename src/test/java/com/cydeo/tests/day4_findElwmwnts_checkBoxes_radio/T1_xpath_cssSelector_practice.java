d1b2eee8d4ae2d99226daedcbe240e4ed312c0ff
Mevlut Doger <mdoger@hotmail.com>
2023-01-02 02:57:00 +0000

    day4 Task cssSelector_getText

--- src/test/java/com/cydeo/tests/day4_findElwmwnts_checkBoxes_radio/T1_xpath_cssSelector_practice.java
+++ src/test/java/com/cydeo/tests/day4_findElwmwnts_checkBoxes_radio/T1_xpath_cssSelector_practice.java
@@ -24,10 +24,13 @@ public class T1_xpath_cssSelector_practice {
 
         //b. “Forgot password” header
         WebElement header_ex1= driver.findElement(By.cssSelector("div.example > h2"));
+        //Locate header using xpath, and using web element text "Forgot Password"
         WebElement header_ex2= driver.findElement(By.xpath("//h2[text()='Forgot Password']"));
         // c. “E-mail” text
-
+        WebElement emailLabel= driver.findElement(By.xpath("//label[@for='email']"));
         //d. E-mail input box
+        WebElement inputBox_ex1=driver.findElement(By.xpath("//input[@name='email']"));
+
         // e. “Retrieve password” button f. “Powered by Cydeo text
         //4. Verify all web elements are displayed.
         //First solve the task with using cssSelector only. Try to create 2 different
