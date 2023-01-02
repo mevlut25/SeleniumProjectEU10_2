7b464cef49b6201573e204c57f2b724cd3bb0815
Mevlut Doger <mdoger@hotmail.com>
2023-01-02 04:44:30 +0000

    day4 Task cssSelector_getText

--- src/test/java/com/cydeo/tests/day4_findElwmwnts_checkBoxes_radio/T1_xpath_cssSelector_practice.java
+++ src/test/java/com/cydeo/tests/day4_findElwmwnts_checkBoxes_radio/T1_xpath_cssSelector_practice.java
@@ -16,23 +16,35 @@ public class T1_xpath_cssSelector_practice {
         //3. Locate all the WebElements on the page using XPATH and/or CSS locator only (total of 6)
         //a. “Home” link
         //Locate homeLink using cssSelector
-        WebElement homeLink_ex1 = driver.findElement(By.cssSelector("a[class='nav-Link']"));
+        //WebElement homeLink_ex1 = driver.findElement(By.cssSelector("a[class='nav-link']"));
         //Locate homelink using cssSelector syntax #2
-        WebElement homeLink_ex2 = driver.findElement(By.cssSelector("a.nav_Link"));
+        WebElement homeLink_ex2 = driver.findElement(By.cssSelector("a.nav-link"));
 
         WebElement homeLink_ex3 = driver.findElement(By.cssSelector("a[href='/']"));
 
         //b. “Forgot password” header
-        WebElement header_ex1= driver.findElement(By.cssSelector("div.example > h2"));
+        WebElement header_ex1 = driver.findElement(By.cssSelector("div.example > h2"));
         //Locate header using xpath, and using web element text "Forgot Password"
-        WebElement header_ex2= driver.findElement(By.xpath("//h2[text()='Forgot Password']"));
+        WebElement header_ex2 = driver.findElement(By.xpath("//h2[text()='Forgot Password']"));
         // c. “E-mail” text
-        WebElement emailLabel= driver.findElement(By.xpath("//label[@for='email']"));
+        WebElement emailLabel = driver.findElement(By.xpath("//label[@for='email']"));
         //d. E-mail input box
-        WebElement inputBox_ex1=driver.findElement(By.xpath("//input[@name='email']"));
+        WebElement inputBox_ex1 = driver.findElement(By.xpath("//input[@name='email']"));
+        WebElement inputBox_ex2 = driver.findElement(By.xpath("//input[contains(@pattern,'a-z')]"));
 
-        // e. “Retrieve password” button f. “Powered by Cydeo text
+        // e. “Retrieve password” button
+        WebElement retrievePasswordBtn = driver.findElement(By.xpath("//button [@type= 'submit']"));
+        //f. “Powered by Cydeo text
+        WebElement powerByCydeoText = driver.findElement(By.xpath("//div[@style='text-align: center;']"));
+
         //4. Verify all web elements are displayed.
+        System.out.println("homeLink_ex2.isDisplayed() = " + homeLink_ex2.isDisplayed());
+        System.out.println("header_ex2.isDisplayed() = " + header_ex2.isDisplayed());
+        System.out.println("emailLabel.isDisplayed() = " + emailLabel.isDisplayed());
+        System.out.println("retrievePasswordBtn.isDisplayed() = " + retrievePasswordBtn.isDisplayed());
+        System.out.println("inputBox_ex1.isDisplayed() = " + inputBox_ex1.isDisplayed());
+        System.out.println("powerByCydeoText.isDisplayed() = " + powerByCydeoText.isDisplayed());
+
         //First solve the task with using cssSelector only. Try to create 2 different
         //cssSelector if possible
         //Then solve the task using XPATH only. Try to create 2 different XPATH locator if possible
---
+++ src/test/java/com/cydeo/tests/day4_findElwmwnts_checkBoxes_radio/T4_findElements.java
@@ -0,0 +1,28 @@
+package com.cydeo.tests.day4_findElwmwnts_checkBoxes_radio;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class T4_findElements {
+    public static void main(String[] args) {
+        //TC #4: FindElements Task
+        //1- Open a Chrome browser
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+       //2- Go to: https://practice.cydeo.com/abtest
+        driver.get("https://practice.cydeo.com/abtest");
+        //3- Locate all the links in the page.
+        List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+        //4- Print out the number of the links on the page.
+        System.out.println("allLinks.size() = " + allLinks.size());
+        // 5- Print out the texts of the links.
+        for (WebElement each : allLinks) {
+            System.out.println("Text of link: "+ each.getText());
+        }
+        //6- Print out the HREF attribute values of the links
+    }
+}
