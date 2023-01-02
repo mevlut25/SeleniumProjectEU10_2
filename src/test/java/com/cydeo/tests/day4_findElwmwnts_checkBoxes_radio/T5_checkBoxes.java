d60459ac5733e34fbbdb7a419aa93bacb744c1cb
Mevlut Doger <mdoger@hotmail.com>
2023-01-02 05:36:32 +0000

    day4 Task cssSelector_getText

--- src/test/java/com/cydeo/tests/day4_findElwmwnts_checkBoxes_radio/T5_checkBoxes.java
+++ src/test/java/com/cydeo/tests/day4_findElwmwnts_checkBoxes_radio/T5_checkBoxes.java
@@ -10,13 +10,13 @@ import java.util.concurrent.TimeUnit;
 public class T5_checkBoxes {
     public static void main(String[] args) throws InterruptedException {
         //. Go to http://practice.cydeo.com/checkboxes
-        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://practice.cydeo.com/checkboxes");
         //We have to locate checkboxes
-        WebElement checkbox1= driver.findElement(By.xpath("//input[@name='checkbox1']"));
-        WebElement checkbox2= driver.findElement(By.xpath("//input[@name='checkbox2']"));
+        WebElement checkbox1 = driver.findElement(By.xpath("//input[@name='checkbox1']"));
+        WebElement checkbox2 = driver.findElement(By.xpath("//input[@name='checkbox2']"));
         //2. Confirm checkbox #1 is NOT selected by default
         System.out.println("checkbox1.isSelected() = " + checkbox1.isSelected());
         // 3. Confirm checkbox #2 is SELECTED by default.
@@ -30,6 +30,9 @@ public class T5_checkBoxes {
         Thread.sleep(2000);
         checkbox2.click();
         //6. Confirm checkbox #1 is SELECTED.
+        System.out.println("checkbox1.isSelected(), expected true = " + checkbox1.isSelected());
+
         //7. Confirm checkbox #2 is NOT selected.
+        System.out.println("checkbox2.isSelected(), expected false = " + checkbox2.isSelected());
     }
 }
