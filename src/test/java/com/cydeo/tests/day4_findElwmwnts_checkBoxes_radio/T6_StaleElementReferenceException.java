a35f1151216d6a74bd2598ae1b12b7fb0130244e
Mevlut Doger <mdoger@hotmail.com>
2023-01-03 01:38:08 +0000

    day4 Task cssSelector_getText

--- src/test/java/com/cydeo/tests/day4_findElwmwnts_checkBoxes_radio/T6_StaleElementReferenceException.java
+++ src/test/java/com/cydeo/tests/day4_findElwmwnts_checkBoxes_radio/T6_StaleElementReferenceException.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day4_findElwmwnts_checkBoxes_radio;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,9 +15,14 @@ public class T6_StaleElementReferenceException {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         //2- Go to: https://practice.cydeo.com/abtest
         driver.get("https://practice.cydeo.com/abtest");
+        WebElement cydeoLink= driver.findElement(By.xpath("//a[text()='CYDEO']"));
         // 3- Locate “CYDEO” link, verify it is displayed.
+        System.out.println("cydeoLink.isDisplayed() = " + cydeoLink.isDisplayed());
         // 4- Refresh the page.
+        driver.navigate().refresh();
         //5- Verify it is displayed, again.
+        cydeoLink= driver.findElement(By.xpath("//a[text()='CYDEO']"));
+        System.out.println("cydeoLink.isDisplayed() = " + cydeoLink.isDisplayed());
 
     }
 }
