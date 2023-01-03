2caf93c990b451f377c3fbfc894c048c6c501b09
Mevlut Doger <mdoger@hotmail.com>
2023-01-03 17:41:27 +0000

    day4 Task cssSelector_getText

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T3_RadioButton_cont.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T3_RadioButton_cont.java
@@ -18,20 +18,24 @@ public class T3_RadioButton_cont {
         driver.get("https://practice.cydeo.com/radio_buttons");
         //3. Click to “Hockey” radio button
         //Locate name='sport' radio button and store them in a lsit os Web Element.
-        List<WebElement> sportRadioButtons = driver.findElements(By.name("sport"));
-        for (WebElement each : sportRadioButtons) {
+        clickAndVerificationRadioButton(driver,"sport","hockey");
+        clickAndVerificationRadioButton(driver,"sport","football");
+
+
+    }
+
+    private static void clickAndVerificationRadioButton(WebDriver driver, String nameAttribute, String idValue) {
+        List<WebElement> RadioButtons = driver.findElements(By.name(nameAttribute));
+        //3. Click to “Hockey” radio button
+        //Locate name='sport' radio button and store them in a list os Web Element.
+        for (WebElement each : RadioButtons) {
             String eachId = each.getAttribute("id");
             System.out.println("eachId = " + eachId);
-            if (eachId.equals("hockey")) {
+            if (eachId.equals(idValue)) {
                 each.click();
-                System.out.println("Hockey is selected : = "+each.isSelected());
+                System.out.println(eachId+" is selected : = " + each.isSelected());
                 break;
-
             }
-
         }
-
-        //4. Verify “Hockey” radio button is selected after clicking.
-
     }
 }
