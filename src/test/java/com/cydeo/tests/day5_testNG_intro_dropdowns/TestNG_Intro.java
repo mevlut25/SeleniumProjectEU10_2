974575e0fd212e219a2dbee822139768e34cfd04
Mevlut Doger <mdoger@hotmail.com>
2023-01-03 17:50:33 +0000

    day4 Task cssSelector_getText

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -1,13 +1,27 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestNG_Intro {
-      @Test
-    public void test1(){
-          System.out.println("Test 1 is running...");
+    @BeforeMethod
+    public void setUpMethod() {
+        System.out.println("-->Before Method Is Running!");
     }
-    public void test2(){
+
+    @AfterMethod
+    public void tearDownMethod() {
+        System.out.println("--> After Method Is Running!!! ");
+    }
+
+    @Test
+    public void test1() {
+        System.out.println("Test 1 is running...");
+    }
+
+    @Test
+    public void test2() {
         System.out.println("Test 2 is running...");
     }
 }
