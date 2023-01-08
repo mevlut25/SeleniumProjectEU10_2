642e7ed1506dc240df94cf863af2888501d2f57e
Mevlut Doger <mdoger@hotmail.com>
2023-01-08 03:44:23 +0000

    day6 Task6 date dropdowns

---
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T1_Alert_Practices.java
@@ -0,0 +1,54 @@
+package com.cydeo.tests.day6_alerts_iframes_windows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T1_Alert_Practices {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod() {
+        //1. Open browser
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //2. Go to website: http://practice.cydeo.com/javascript_alerts
+        driver.get("http://practice.cydeo.com/javascript_alerts");
+    }
+
+    @Test
+    public void alert_test() throws InterruptedException {
+
+        //3. Click to “Click for JS Alert” button
+        WebElement informationAlertButton=driver.findElement(By.xpath("//button[.='Click for JS Alert']"));
+        Thread.sleep(2000);
+        informationAlertButton.click();
+        //To be able to click to Alert OK button we need to switch driver's focus to Alert itself.
+        Alert alert=driver.switchTo().alert();
+        //4. Click to OK button from the alert
+        alert.accept();
+        //5. Verify “You successfully clicked an alert” text is displayed.
+        WebElement resultText=driver.findElement(By.xpath("//p[@id='result']"));
+        //Failure message will only be displayed if assertion fails: "Result text is NOT displayed"
+        Assert.assertTrue(resultText.isDisplayed(),"Result is not displayed");
+        String expectedText = "You successfully clicked an alert";
+        String actualText = resultText.getText();
+        Assert.assertEquals(actualText,expectedText,"Actual result text is not as expected");
+    }
+
+}
+
+
+
+
+
+
+
