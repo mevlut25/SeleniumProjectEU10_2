fc4b2526539694c3d19663fc630df393243302d5
Mevlut Doger <mdoger@hotmail.com>
2023-01-08 14:02:26 +0000

    day6 Task6 date dropdowns

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T4_IframePractice.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T4_IframePractice.java
@@ -26,14 +26,21 @@ public class T4_IframePractice {
     public void iframe_test(){
         //We need to switch driver's focus to iframe
         //option #1-switching to iframe using id attribute value
-        driver.switchTo().frame("mce_0_ifr");
+        //driver.switchTo().frame("mce_0_ifr");
+        //option #2-passing index number of frame
+        //driver.switchTo().frame(0);
+        //option #3- locate as web element and pass in frame() method
+        driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@id='mce_0_ifr']")));
         //Locate the p tag
         WebElement yourContentGoesHereText=driver.findElement(By.xpath("//p"));
-        Assert.assertTrue(yourContentGoesHereText.isDisplayed());
-
-
         //4. Assert: “Your content goes here.” Text is displayed.
+        Assert.assertTrue(yourContentGoesHereText.isDisplayed());
         //5. Assert: “An iFrame containing the TinyMCE WYSIWYG Editor”
+        //verify "An iframe containing the TinyMCE WYSIWYG editor"
+        driver.switchTo().parentFrame();
+
+        WebElement headerText=driver.findElement(By.xpath("//h3"));
+        Assert.assertTrue(headerText.isDisplayed());
 
 
     }
