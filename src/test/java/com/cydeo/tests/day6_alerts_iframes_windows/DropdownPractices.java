5ffc72cf969f8e2d24419d3ef5ebee022a94b24c
Mevlut Doger <mdoger@hotmail.com>
2023-01-08 01:35:02 +0000

    day4 Task cssSelector_getText

---
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
@@ -0,0 +1,19 @@
+package com.cydeo.tests.day6_alerts_iframes_windows;
+
+import org.testng.annotations.Test;
+
+public class DropdownPractices {
+
+
+    @Test
+    public void dropdown_task5(){
+        //1. Open Chrome browser
+        //2. Go to http://practice.cybertekschool.com/dropdown
+        //3. Select Illinois
+        //4. Select Virginia
+        //5. Select California
+        //6. Verify final selected option is California.
+        //Use all Select options. (visible text, value, index)
+
+    }
+}
