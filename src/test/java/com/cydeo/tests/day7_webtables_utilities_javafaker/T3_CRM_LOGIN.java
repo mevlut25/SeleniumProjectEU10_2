7215c7eefb3a4e16f2750c8003002464b4204162
Mevlut Doger <mdoger@hotmail.com>
2023-01-16 03:34:49 +0000

    day7 T3_CRM_LOGIN

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
@@ -1,9 +1,8 @@
 package com.cydeo.tests.day7_webtables_utilities_javafaker;
 
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.WebDriverFactory;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -22,17 +21,10 @@ public class T3_CRM_LOGIN {
         //1. Create new test and make setups
         //2. Goto:https://login1.nextbasecrm.com/
         driver.get("https://login1.nextbasecrm.com");
-        //3. Enter valid username
-        WebElement inputUsername=driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
-        inputUsername.sendKeys("helpdesk1@cybertekschool.com");
 
-        //4. Enter valid password
-        WebElement inputPassword=driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
-        inputPassword.sendKeys("UserUser");
-        //5. Click to`LogIn`button
-        WebElement loginButton=driver.findElement(By.xpath("//input[@name='Log In']"));
-        loginButton.click();
         //6. Verify title is as expected:
+        BrowserUtils.verifyTitle(driver,"My tasks");
+        System.out.println(driver.getTitle());
 
 
 
---
+++ src/test/java/com/cydeo/utilities/CRM_Utilities.java
@@ -0,0 +1,25 @@
+package com.cydeo.utilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CRM_Utilities {
+    /*
+    This is method will login with helpdesk1@cybertekschool.com
+    user when it is called
+     */
+    public static void crm_login(WebDriver driver){
+        //3. Enter valid username
+        WebElement inputUsername=driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        inputUsername.sendKeys("helpdesk1@cybertekschool.com");
+
+        //4. Enter valid password
+        WebElement inputPassword=driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        inputPassword.sendKeys("UserUser");
+        //5. Click to`LogIn`button
+        WebElement loginButton=driver.findElement(By.xpath("//input[@value='Log In']"));
+        loginButton.click();
+
+    }
+}
