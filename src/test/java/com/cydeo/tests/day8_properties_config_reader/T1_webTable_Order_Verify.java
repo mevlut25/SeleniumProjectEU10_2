3a9b959c18cce8c0ae83307df1f044311db2930a
Mevlut Doger <mdoger@hotmail.com>
2023-01-16 04:54:18 +0000

    day8 T1_webTable_Order_Verify

--- src/test/java/com/cydeo/tests/day8_properties_config_reader/T1_webTable_Order_Verify.java
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/T1_webTable_Order_Verify.java
@@ -1,5 +1,35 @@
 package com.cydeo.tests.day8_properties_config_reader;
 
-public class T1_webTable_Order_Verify {
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-}
+import java.util.concurrent.TimeUnit;
+
+public class T1_webTable_Order_Verify {
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod() {
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //1. Goto:https://practice.cydeo.com/web-tables
+        driver.get("https://practice.cydeo.com/web-tables");
+    }
+        @Test
+        public void order_name_verify_test () {
+        //Locate the cell that has Bob Martin text in it
+            WebElement bobMartinCell=driver.findElement(By.xpath("/table[@id='ctl00_MainContent_orderGrid']//td[.='Bob Martin']"));
+            //2. VerifyBob’s name is listed as expected.
+            //Expected: “Bob Martin”
+            //3. VerifyBobMartin’s order date is as expected
+            // Expected: 12/31/2021
+        }
+    }
+
+
+
